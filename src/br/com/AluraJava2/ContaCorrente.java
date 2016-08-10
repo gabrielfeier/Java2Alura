1f78e0828e7afa113ff8b8acf76ae6d36bd29fc3
GabrielFeier <gabriel.feier@hotmail.com>
2016-08-10 17:07:39 -0300

    first alt

--- src/br/com/AluraJava2/ContaCorrente.java
+++ src/br/com/AluraJava2/ContaCorrente.java
@@ -3,7 +3,7 @@ package br.com.AluraJava2;
 public class ContaCorrente extends Conta{
 	
     public void atualiza(double taxa) {
-        this.saldo += this.saldo * taxa * 2;
+        this.saldo += this.saldo * taxa * 2 +2;
     }
     
 }
